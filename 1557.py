2e96bdf47aef59da2e96927de6606258243ab6a1
MichaelPereira31 <michaelpereira408@gmail.com>
2020-09-30 14:48:23 -0300

    Update 1557.py

--- 1557.py
+++ 1557.py
@@ -1,33 +1,32 @@
-n =1
+N =1
 
-while n >=1:
-    n = int(input())
+while N >=1:
+    N = int(input())
 
-    if n > 0:
+    if N > 0:
         matriz = []
 
-        for i in range(n):
+        for i in range(N):
             matriz.append([])
-
-            for j in range(n):
+            for j in range(N):
                 matriz[i].append(0)
 
 
         matriz[0][0] = 1
-        for i in range(0,n):
+        for i in range(0,N):
             if i >=1:
                 matriz[i][0] = matriz[i - 1][0] * 2
             
-            for j in range(1, n):
+            for j in range(1, N):
                 matriz[i][j] = matriz[i][j-1] * 2
 
-        T = len(str(matriz[n-1][n-1]))
-        for i in range(n):
-            for j in range(n):
+        T = len(str(matriz[N-1][N-1]))
+        for i in range(N):
+            for j in range(N):
                 matriz[i][j] = str(matriz[i][j])
                 while len(matriz[i][j]) < T:
                     matriz[i][j] = ' ' + matriz[i][j]
-            M = ' '.join(matriz[i])
+            R = ' '.join(matriz[i])
         
-            print(M)
+            print(R)
         print()
