844e42e5d796708f7c007c12b539643fb8b2df7a
MichaelPereira31 <michaelpereira408@gmail.com>
2020-10-02 21:28:03 -0300

    Questão Finalizada

--- 1848.py
+++ 1848.py
@@ -1,15 +1,17 @@
-n = 0
 valor = 0
-while n<3:
-    entrada =  input()
-    if(entrada == 'caw caw'):
-        print(valor)
-        valor = 0
-        n += 1
-    for i in range(3):
-        if (entrada[i] == '*' or entrada[i] == '-'):
-            if(entrada[i] == '*'):
-                if( i == 2):
-                    valor += 1
-                elif( i == 0):
-                    valor += 4
+c = 0
+while(c<3):
+  n = input()
+  if(n != 'caw caw'):
+      
+    if(n[0] == '*'):
+      valor += 4
+    if(n[1] == '*'):
+      valor += 2
+    if(n[2] == '*'):
+      valor += 1
+  else:
+      print(valor)
+      valor = 0
+      c += 1
+    
